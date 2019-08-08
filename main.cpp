ccdd69339490db66aace3ad27bb7df2d86d31bac
Rizwan <rizowan69@gmail.com>
2019-08-08 22:02:03 +0600

    lllllll
    lllllll

--- main.cpp
+++ main.cpp
@@ -5,47 +5,24 @@ void init(){
     glOrtho(-30, 30, -25, 25, -10, 10);
 }
 
-void blabla(int x, int y){
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_QUADS);
-        glVertex2f(x-1.5, y+3.8);   //full quad
-        glVertex2f(x+1.5, y+3.8);
-        glVertex2f(x+1, y+0);
-        glVertex2f(x-1, y+0);
-    glEnd();
-    glFlush();
-
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_QUADS);
-        glVertex2f(x-1, y+0);   //full quad
-        glVertex2f(x+1, y+0);
-        glVertex2f(x+1.5, y-3.8);
-        glVertex2f(x-1.5, y-3.8);
-    glEnd();
-    glFlush();
-
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_QUADS);
-        glVertex2f(x+0, y+1);   //full quad
-        glVertex2f(x+3.8, y+1.5);
-        glVertex2f(x+3.8, y-1.5);
-        glVertex2f(x+0, y-1);
-    glEnd();
-    glFlush();
-
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_QUADS);
-        glVertex2f(x+0, y+1);   //full quad
-        glVertex2f(x-3.8, y+1.5);
-        glVertex2f(x-3.8, y-1.5);
-        glVertex2f(x+0, y-1);
+void triangle(float a,float b,float c, float d, float e, float f){
+    glBegin(GL_TRIANGLES);
+        glVertex2f(a,b);
+        glVertex2f(c,d);
+        glVertex2f(e,f);
     glEnd();
     glFlush();
 }
 
+void star(float x, float y){
+    triangle(x-5,y+3, x+5,y+3, x+0,y-1.3 );
+    triangle(x-4,y-4, x+2,y+0, x+0,y+6 );
+    triangle(x+4,y-4, x-2,y+0, x+0,y+6 );
+}
+
 void draw(){
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f, 1.0f, 1.0f);
+    glColor3f(0, 0.149, 0.564);
     glBegin(GL_QUADS);
         glVertex2f(-25, 15);   //full quad
         glVertex2f(25, 15);
@@ -54,28 +31,30 @@ void draw(){
     glEnd();
     glFlush();
 
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1,1,1);
     glBegin(GL_QUADS);
-        glVertex2f(-25, 4);   //full quad
-        glVertex2f(25, 4);
-        glVertex2f(25, -4);
-        glVertex2f(-25, -4);
+        glVertex2f(-25, 9);   //full quad
+        glVertex2f(25, 9);
+        glVertex2f(25, 3);
+        glVertex2f(-25,3);
     glEnd();
     glFlush();
 
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1,1,1);
     glBegin(GL_QUADS);
-        glVertex2f(-4, 15);   //full quad
-        glVertex2f(4, 15);
-        glVertex2f(4, -15);
-        glVertex2f(-4, -15);
+        glVertex2f(-25, -3);   //full quad
+        glVertex2f(25, -3);
+        glVertex2f(25, -9);
+        glVertex2f(-25, -9);
     glEnd();
     glFlush();
 
-    blabla(-15,9.8);
-    blabla(15,9.8);
-    blabla(15,-9.8);
-    blabla(-15,-9.8);
+    glColor3f(1.0, 0.047, 0.152);
+    triangle(-25,15, -2,0, -25,-15);
+
+    //star
+    glColor3f(1,1,1);
+    star(-17,0);
 
     glutSwapBuffers();
 }
@@ -83,7 +62,7 @@ int main(){
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(900, 700);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Georgia");
+    glutCreateWindow("Cuba");
     init();
     glutDisplayFunc(draw);
     glutMainLoop();
